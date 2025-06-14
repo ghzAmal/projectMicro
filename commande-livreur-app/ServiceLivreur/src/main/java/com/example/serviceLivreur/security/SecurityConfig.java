78cb6ea756c523cd66c815d529bdd4072f1c6141
amal ghozzi <ghozzi.amal@gtiinfo.com.tn>
2025-06-14 06:46:29 +0100

    c

--- commande-livreur-app/ServiceLivreur/src/main/java/com/example/serviceLivreur/security/SecurityConfig.java
+++ commande-livreur-app/ServiceLivreur/src/main/java/com/example/serviceLivreur/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,17 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        System.out.println("⚠️ SecurityFilterChain ACTIVE");
-
-
-        http
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().authenticated() // Exige token valide
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt())  // JWT token validation
-                .csrf().disable();
-
-        return http.build();
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        String issuerUri = "http://host.docker.internal:8080/realms/projectmicro";
+        // On construit un ReactiveJwtDecoder via l'issuer URI
+        return NimbusReactiveJwtDecoder.withJwkSetUri(
+                issuerUri + "/protocol/openid-connect/certs").build();
     }
 }
