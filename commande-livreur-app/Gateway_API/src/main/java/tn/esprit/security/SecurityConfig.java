3f0ccfa6616e4e4f1ffe723e423fe24defa87134
amal ghozzi <ghozzi.amal@gtiinfo.com.tn>
2025-06-14 05:30:12 +0100

    lk

--- commande-livreur-app/Gateway_API/src/main/java/tn/esprit/security/SecurityConfig.java
+++ commande-livreur-app/Gateway_API/src/main/java/tn/esprit/security/SecurityConfig.java
@@ -2,25 +2,20 @@ package tn.esprit.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/commandes/**").authenticated()  // nécessite un token
-                        .anyRequest().denyAll() // bloque toute autre requête
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt()) // Active le serveur ressource JWT
-                .csrf().disable(); // désactive CSRF pour les APIs
-
-        return http.build();
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        String issuerUri = "http://host.docker.internal:8080/realms/projectmicro";
+        // On construit un ReactiveJwtDecoder via l'issuer URI
+        return NimbusReactiveJwtDecoder.withJwkSetUri(
+                issuerUri + "/protocol/openid-connect/certs").build();
     }
 }
-
-
-
